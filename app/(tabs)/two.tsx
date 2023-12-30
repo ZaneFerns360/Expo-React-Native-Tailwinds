9d4f38118a963b891171944915a29629cac1a386
ZaneFerns360 <zane.ferns360@gmail.com>
2023-12-30 19:13:41 +0530

    getting started

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,22 @@
-import { StyleSheet } from 'react-native';
+import React from 'react'
+import { Text as RNText, View as RNView } from '../../components/Themed'
+import { styled } from 'nativewind'
+import EditScreenInfo from '../../components/EditScreenInfo'
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+const Text = styled(RNText)
+const View = styled(RNView)
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-lg font-bold">Tab Two</Text>
+      <Text className="text-3xl text-red-400">Hello world.</Text>
+      <View
+        className="my-8 h-px w-4/5"
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
